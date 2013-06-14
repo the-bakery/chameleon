ebebc82a39b80382fea068b142b14c4b01fb285a
Mate J Kovacs <mkovaxx@gmail.com>
2013-06-13 23:13:13 -0700

    factor out VideoStream, ignore .swp

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ from pyglet.gl import *
 import cv2
 from cv2 import VideoCapture
 
+from videostream import VideoStream
+
 
 class Main(pyglet.window.Window):
 
     def __init__(self, **kwargs):
         super(Main, self).__init__(**kwargs)
-	self.streams = [VideoCapture(1)]
-	assert self.streams[0].isOpened(), 'failed to open stream 0'
-	self.streams[0].set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 720)
-	self.streams[0].set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 540)
-	self.textures = (pyglet.gl.GLuint * 2)()
-	glGenTextures(2, self.textures)
-
+	self.streams = [VideoStream(0, 1024, 768)]
+	
     def on_resize(self, width, height):
         glViewport(0, 0, width, height)
         glMatrixMode(GL_PROJECTION)
@@ -29,28 +26,24 @@ class Main(pyglet.window.Window):
         return pyglet.event.EVENT_HANDLED
 
     def on_idle(self, dt):
-    	status, frame = self.streams[0].read()
-	if status:
-	    width = frame.shape[1]
-	    height = frame.shape[0]
-	    glBindTexture(GL_TEXTURE_2D, self.textures[0])
-	    glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE)
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-	    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-	    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, frame.ctypes.data)
-
+    	for stream in self.streams:
+	    stream.next()
+    	
     def on_draw(self):
 	glClear(GL_COLOR_BUFFER_BIT)
+	def quad(x0, y0, x1, y1, du, dv):
+	    glTexCoord2f(0.0 + du, 0.0 + dv)
+	    glVertex2f(x0, y0)
+	    glTexCoord2f(1.0 + du, 0.0 + dv)
+    	    glVertex2f(x1, y0)
+	    glTexCoord2f(1.0 + du, 1.0 + dv)
+    	    glVertex2f(x1, y1)
+	    glTexCoord2f(0.0 + du, 1.0 + dv)
+    	    glVertex2f(x0, y1)
         glBegin(GL_QUADS)
-    	glTexCoord2f(0.0, 0.0)
-	glVertex2f(0.0, 0.0)
-	glTexCoord2f(1.0, 0.0)
-    	glVertex2f(1.0, 0.0)
-	glTexCoord2f(1.0, 1.0)
-    	glVertex2f(1.0, 1.0)
-	glTexCoord2f(0.0, 1.0)
-    	glVertex2f(0.0, 1.0)
-    	glEnd()
+	quad(0.0, 0.0, 0.5, 1.0, -0.125, 0.0)
+    	quad(0.5, 0.0, 1.0, 1.0, +0.125, 0.0)
+	glEnd()
 
     def on_mouse_press(self, x, y, button, modifiers):
         return
