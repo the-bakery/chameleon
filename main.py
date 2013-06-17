a8f7e28d83684159ccd69bc8b0f7f0d00e189827
Simon Vana <simon.vana@gmail.com>
2013-06-17 12:43:09 -0700

    Change all tabs to spaces

--- main.py
+++ main.py
@@ -11,39 +11,39 @@ class Main(pyglet.window.Window):
 
     def __init__(self, **kwargs):
         super(Main, self).__init__(**kwargs)
-	self.streams = [VideoStream(0, 1024, 768)]
-	
+        self.streams = [VideoStream(0, 1024, 768)]
+    
     def on_resize(self, width, height):
         glViewport(0, 0, width, height)
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-	glOrtho(0.0, 1.0, 1.0, 0.0, -1, 1);
+        glOrtho(0.0, 1.0, 1.0, 0.0, -1, 1);
         glMatrixMode(GL_MODELVIEW)
-	glLoadIdentity()
+        glLoadIdentity()
         glDisable(GL_DEPTH_TEST)
-	glEnable(GL_TEXTURE_2D)
-	glClearColor(0.0, 0.0, 0.0, 1.0)
+        glEnable(GL_TEXTURE_2D)
+        glClearColor(0.0, 0.0, 0.0, 1.0)
         return pyglet.event.EVENT_HANDLED
 
     def on_idle(self, dt):
-    	for stream in self.streams:
-	    stream.next()
-    	
+        for stream in self.streams:
+            stream.next()
+        
     def on_draw(self):
-	glClear(GL_COLOR_BUFFER_BIT)
-	def quad(x0, y0, x1, y1, du, dv):
-	    glTexCoord2f(0.0 + du, 0.0 + dv)
-	    glVertex2f(x0, y0)
-	    glTexCoord2f(1.0 + du, 0.0 + dv)
-    	    glVertex2f(x1, y0)
-	    glTexCoord2f(1.0 + du, 1.0 + dv)
-    	    glVertex2f(x1, y1)
-	    glTexCoord2f(0.0 + du, 1.0 + dv)
-    	    glVertex2f(x0, y1)
+        glClear(GL_COLOR_BUFFER_BIT)
+        def quad(x0, y0, x1, y1, du, dv):
+            glTexCoord2f(0.0 + du, 0.0 + dv)
+            glVertex2f(x0, y0)
+            glTexCoord2f(1.0 + du, 0.0 + dv)
+            glVertex2f(x1, y0)
+            glTexCoord2f(1.0 + du, 1.0 + dv)
+            glVertex2f(x1, y1)
+            glTexCoord2f(0.0 + du, 1.0 + dv)
+            glVertex2f(x0, y1)
         glBegin(GL_QUADS)
-	quad(0.0, 0.0, 0.5, 1.0, -0.125, 0.0)
-    	quad(0.5, 0.0, 1.0, 1.0, +0.125, 0.0)
-	glEnd()
+        quad(0.0, 0.0, 0.5, 1.0, -0.125, 0.0)
+        quad(0.5, 0.0, 1.0, 1.0, +0.125, 0.0)
+        glEnd()
 
     def on_mouse_press(self, x, y, button, modifiers):
         return
--- videostream.py
+++ videostream.py
@@ -9,23 +9,23 @@ class VideoStream(object):
 
     def __init__(self, device, width, height):
         self.capture = VideoCapture(device)
-	assert self.capture.isOpened(), 'failed to open stream %s' % device
-	self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
-	self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
-	self.texture = pyglet.gl.GLuint()
-	glGenTextures(1, self.texture)
-	glBindTexture(GL_TEXTURE_2D, self.texture)
-	glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
+        assert self.capture.isOpened(), 'failed to open stream %s' % device
+        self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
+        self.capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
+        self.texture = pyglet.gl.GLuint()
+        glGenTextures(1, self.texture)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
 
     def next(self):
         status, frame = self.capture.read()
-	if status:
-	    width = frame.shape[1]
-	    height = frame.shape[0]
-	    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, frame.ctypes.data)
-	    glBindTexture(GL_TEXTURE_2D, self.texture)
+        if status:
+            width = frame.shape[1]
+            height = frame.shape[0]
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, frame.ctypes.data)
+            glBindTexture(GL_TEXTURE_2D, self.texture)
 
